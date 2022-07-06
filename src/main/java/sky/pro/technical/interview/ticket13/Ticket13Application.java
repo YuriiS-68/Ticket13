23354613d4eb585ed227dc44053860f89040a471
Yuvis <yurii_s@bigmir.net>
2022-07-06 13:30:23 +0300

    init commit

---
+++ src/main/java/sky/pro/technical/interview/ticket13/Ticket13Application.java
@@ -0,0 +1,13 @@
+package sky.pro.technical.interview.ticket13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ticket13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ticket13Application.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/technical/interview/ticket13/Ticket13ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.technical.interview.ticket13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ticket13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
